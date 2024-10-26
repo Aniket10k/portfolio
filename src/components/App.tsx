5a967ef3b8336749f7134a861896f038bb9b83eb
Aniket Kulkarni <aniketkulkarni@Anikets-MacBook-Air.local>
2024-10-26 19:38:58 +0530

    Added personal photo.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import Typography from "@mui/material/Typography";
+import image from './me.png';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={image} alt="Aniket Kulkarni" width="250" height="250"/>
         <Typography variant={"h1"}>Aniket Kulkarni</Typography>
         <Typography variant={"h5"}>
           Personal portfolio!!
