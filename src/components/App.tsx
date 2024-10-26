0a945e379fef7e0b54d57b2d41d024d9f3400285
Aniket Kulkarni <aniketkulkarni@Anikets-MacBook-Air.local>
2024-10-26 19:08:34 +0530

    Initial commit with gh-pages

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import Typography from "@mui/material/Typography";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Typography variant={"h1"}>Aniket Kulkarni</Typography>
+        <Typography variant={"h5"}>
+          Personal portfolio!!
+        </Typography>
+      </header>
+    </div>
+  );
+}
+
+export default App;
